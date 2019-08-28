b2b65d65208bc7f6dee98c11d9973a8d89607868
Mithun Ayachit <mithun.ayachit@propellerhealth.com>
2019-08-28 05:42:34 -0500

    Add tools and configs to use github package registry

--- README.md
+++ README.md
@@ -1,21 +1,16 @@
-# express-statsd [![Build status](https://travis-ci.org/uber/express-statsd.png?branch=master)](https://travis-ci.org/uber/express-statsd)
+# express-statsd
 
-[StatsD](https://github.com/etsy/statsd/) route monitoring middleware for 
-[Connect](https://github.com/senchalabs/connect)/[Express](https://github.com/visionmedia/express).
-This middleware can be used either globally or on a per-route basis (preferred)
-and sends status codes and response times to StatsD.
+[StatsD](https://github.com/etsy/statsd/) route monitoring middleware for [Connect](https://github.com/senchalabs/connect)/[Express](https://github.com/visionmedia/express). This middleware can be used either globally or on a per-route basis (preferred) and sends status codes and response times to StatsD.
 
 ## Installation
 
-``` bash
-npm install express-statsd
-```
+Using NPM: `npm install -P -E @asthmapolis/express-statsd@latest`
 
 ## Usage
 
 An example of an express server with express-statsd:
 
-``` js
+```js
 var express = require('express');
 var expressStatsd = require('express-statsd');
 var app = express();
@@ -29,24 +24,16 @@ app.get('/', function (req, res) {
 app.listen(3000);
 ```
 
-By default, the middleware will send `status_code` and `response_time` stats
-for all requests. For example, using the created server above and a request to
-`http://localhost:3000/`, the following stats will be sent:
+By default, the middleware will send `status_code` and `response_time` stats for all requests. For example, using the created server above and a request to `http://localhost:3000/`, the following stats will be sent:
 
-```
-status_code.200:1|c
-response_time:100|ms
-```
+    status_code.200:1|c
+    response_time:100|ms
 
 ### Per route example
 
-However, it's **highly recommended** that you set `req.statsdKey` which
-will be used to namespace the stats. Be aware that stats will only be logged
-once a response has been sent; this means that `req.statsdKey` can be
-set even after the express-statsd middleware was added to the chain. Here's an 
-example of a server set up with a more specific key:
+However, it's **highly recommended** that you set `req.statsdKey` which will be used to namespace the stats. Be aware that stats will only be logged once a response has been sent; this means that `req.statsdKey` can be set even after the express-statsd middleware was added to the chain. Here's an example of a server set up with a more specific key:
 
-``` js
+```js
 var express = require('express');
 var expressStatsd = require('express-statsd');
 var app = express();
@@ -70,10 +57,8 @@ app.listen(3000);
 
 A GET request to `/` on this server would produce the following stats:
 
-```
-http.get.home.status_code.200:1|c
-http.get.home.response_time:100|ms
-```
+    http.get.home.status_code.200:1|c
+    http.get.home.response_time:100|ms
 
 ### Plain http example
 
@@ -95,13 +80,23 @@ http.createServer(function (req, res) {
 
 ## Options
 
-``` js
+```js
 expressStatsd(options);
 ```
 
-- **options** `Object` - Container for settings
-  - **client** `Object` - The statsd client. Defaults to [lynx](https://github.com/dscape/lynx)
-with host set to `127.0.0.1` and port set to `8125`.
-  - **requestKey** `String` - The key on the `req` object at which to grab
-the key for the statsd logs. Defaults to `req.statsdKey`.
-  - **sendStats** `function` - An optional functional defining the operation when interfacing with the statsD client. For instance, you may want to customize throttling.
+-   **options** `Object` - Container for settings
+    -   **client** `Object` - The statsd client. Defaults to [lynx](https://github.com/dscape/lynx)
+        with host set to `127.0.0.1` and port set to `8125`.
+    -   **requestKey** `String` - The key on the `req` object at which to grab
+        the key for the statsd logs. Defaults to `req.statsdKey`.
+    -   **sendStats** `function` - An optional functional defining the operation when interfacing with the statsD client. For instance, you may want to customize throttling.
+
+## ChangeLog
+
+See [`CHANGELOG.md`](https://github.com/Asthmapolis/express-statsd/blob/master/CHANGELOG.md)
+
+## Development
+
+See [`CONTRIBUTING.md`](https://github.com/Asthmapolis/express-statsd/blob/master/CONTRIBUTING.md)
+
+* * *
--- test/express-statsd.js
+++ test/express-statsd.js
@@ -1,135 +1,137 @@
-var expect = require('chai').expect;
-var Lynx = require('lynx');
-var expressStatsd = require('../lib/express-statsd');
-var utils = require('./utils');
+"use strict";
 
-describe('An express server', function () {
+var expect = require("chai").expect;
+var Lynx = require("lynx");
+var expressStatsd = require("../lib/express-statsd");
+var utils = require("./utils");
+
+describe("An express server", function() {
   utils.runStatsd();
 
-  describe('with express-statsd', function () {
-    describe('receiving a request', function () {
+  describe("with express-statsd", function() {
+    describe("receiving a request", function() {
       utils.runServer(1337, [
         expressStatsd(),
-        function (req, res) {
-          res.send(200);
+        function(req, res) {
+          res.sendStatus(200);
         }
       ]);
-      utils.saveRequest('http://localhost:1337');
+      utils.saveRequest("http://localhost:1337");
       utils.getStatsdMessages();
 
-      it('should make a successful request', function () {
+      it("should make a successful request", function() {
         expect(this.err).to.eql(null);
         expect(this.res.statusCode).to.eql(200);
       });
 
-      it('should send two stats', function () {
+      it("should send two stats", function() {
         expect(this.messages).to.have.length(2);
       });
 
-      it('should send status_code stat', function () {
+      it("should send status_code stat", function() {
         expect(this.messages[0]).to.match(/status_code\.200:\d\|c/);
       });
 
-      it('should send response_time stat', function () {
-        expect(this.messages[1]).to.match(/response_time:\d\|ms/);
+      it("should send response_time stat", function() {
+        expect(this.messages[1]).to.match(/response_time:[\d\.]+\|ms/);
       });
 
-      it('should send stats with no key', function () {
+      it("should send stats with no key", function() {
         expect(this.messages[0]).to.match(/^status_code\.200:\d\|c$/);
         expect(this.messages[1]).to.match(/^response_time:\d|ms$/);
       });
     });
 
-    describe('receiving a request to a 500ing endpoint', function () {
+    describe("receiving a request to a 500ing endpoint", function() {
       utils.runServer(1337, [
         expressStatsd(),
-        function (req, res) {
-          res.send(500);
+        function(req, res) {
+          res.sendStatus(500);
         }
       ]);
-      utils.saveRequest('http://localhost:1337');
+      utils.saveRequest("http://localhost:1337");
       utils.getStatsdMessages();
 
-      it('should send two stats', function () {
+      it("should send two stats", function() {
         expect(this.messages).to.have.length(2);
       });
 
-      it('should send 500 status_code stat', function () {
-        expect(this.messages[0]).to.contain('status_code.500');
+      it("should send 500 status_code stat", function() {
+        expect(this.messages[0]).to.contain("status_code.500");
       });
     });
 
-    describe('with an altered statsdKey receiving a request', function () {
+    describe("with an altered statsdKey receiving a request", function() {
       utils.runServer(1337, [
-        function (req, res, next) {
-          req.statsdKey = 'my-key';
+        function(req, res, next) {
+          req.statsdKey = "my-key";
           next();
         },
         expressStatsd(),
-        function (req, res) {
-          res.send(200);
+        function(req, res) {
+          res.sendStatus(200);
         }
       ]);
-      utils.saveRequest('http://localhost:1337');
+      utils.saveRequest("http://localhost:1337");
       utils.getStatsdMessages();
 
-      it('should send stats with altered key', function () {
+      it("should send stats with altered key", function() {
         expect(this.messages[0]).to.match(/^my-key\.status_code\.200:\d\|c$/);
         expect(this.messages[1]).to.match(/^my-key\.response_time:\d|ms$/);
       });
     });
 
-    describe('with a requestKey option receiving a request', function () {
+    describe("with a requestKey option receiving a request", function() {
       utils.runServer(1337, [
-        function (req, res, next) {
-          req.myKey = 'my-key';
+        function(req, res, next) {
+          req.myKey = "my-key";
           next();
         },
-        expressStatsd({ requestKey: 'myKey' }),
-        function (req, res) {
-          res.send(200);
+        expressStatsd({ requestKey: "myKey" }),
+        function(req, res) {
+          res.sendStatus(200);
         }
       ]);
-      utils.saveRequest('http://localhost:1337');
+      utils.saveRequest("http://localhost:1337");
       utils.getStatsdMessages();
 
-      it('should read from that key', function () {
+      it("should read from that key", function() {
         expect(this.messages[0]).to.match(/^my-key\.status_code\.200:\d\|c$/);
         expect(this.messages[1]).to.match(/^my-key\.response_time:\d|ms$/);
       });
     });
 
-    describe('receiving a request with a custom lynx', function () {
+    describe("receiving a request with a custom lynx", function() {
       utils.runServer(1337, [
-        function (req, res, next) {
-          req.statsdKey = 'my-key';
+        function(req, res, next) {
+          req.statsdKey = "my-key";
           next();
         },
-        expressStatsd({client: new Lynx('127.0.0.1', 8125, {scope: 'my-scope'})}),
-        function (req, res) {
-          res.send(200);
+        expressStatsd({ client: new Lynx("127.0.0.1", 8125, { scope: "my-scope" }) }),
+        function(req, res) {
+          res.sendStatus(200);
         }
       ]);
-      utils.saveRequest('http://localhost:1337');
+      utils.saveRequest("http://localhost:1337");
       utils.getStatsdMessages();
 
-      it('should use the custom lynx client', function () {
+      it("should use the custom lynx client", function() {
         expect(this.messages[0]).to.match(/^my-scope\.my-key\.status_code\.200:\d\|c$/);
         expect(this.messages[1]).to.match(/^my-scope\.my-key\.response_time:\d|ms$/);
       });
     });
   });
 
-  describe('without express-statsd receiving a request', function () {
+  describe("without express-statsd receiving a request", function() {
     utils.runServer(1337, [
-      function (req, res) {
-        res.send(200);
+      function(req, res) {
+        res.sendStatus(200);
       }
     ]);
-    utils.saveRequest('http://localhost:1337');
+    utils.saveRequest("http://localhost:1337");
     utils.getStatsdMessages();
 
-    it('should not send stats', function () {
+    it("should not send stats", function() {
       expect(this.messages).to.have.length(0);
     });
   });
